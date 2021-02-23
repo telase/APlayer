0cb570baa358b34a6b54d4cf5b597c71536b6c71
telase <telase@msn.com>
2021-02-23 20:34:38 +0300

    Add files via upload

---
+++ Utils/FileHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeLaSeAPlayer.Utils
+{
+    public class FileHelper
+    {
+        public static readonly string CodecDirName = "codecs";
+        public static readonly string CodecDirPath = Environment.CurrentDirectory;      
+
+        public static List<string> GetAllFiles(string dir)
+        {
+            var list = new List<string>();
+            var dirInfo = new System.IO.DirectoryInfo(dir);
+            var entries = dirInfo.GetFileSystemInfos();
+
+            foreach (var item in entries)
+            {
+                if(item.Attributes == System.IO.FileAttributes.Directory)
+                {
+                    list.AddRange(GetAllFiles(item.FullName));
+                }
+                else
+                {
+                    var extension = System.IO.Path.GetExtension(item.FullName).ToLower();
+                    if (PlayerHelper.IsVideoFormat(extension) == true)
+                    {
+                        list.Add(item.FullName);
+                    }
+                }
+            }
+            return list;
+        }
+
+        public static string [] GetFiles(string dir)
+        {
+            return System.IO.Directory.GetFiles(dir);
+        }
+
+        public static bool CreateDirectory(string dir)
+        {
+            try
+            {
+                if (System.IO.Directory.Exists(dir) == false)
+                {
+                    System.IO.Directory.CreateDirectory(dir);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool RemoveFile(string file)
+        {
+            try
+            {
+                if(System.IO.File.Exists(file))
+                {
+                    System.IO.File.Delete(file);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
